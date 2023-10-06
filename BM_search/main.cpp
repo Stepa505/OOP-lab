8a8cc642a811cdf9c1a0f29724c5e25e2810a6a4
Stepa505 <stupanum2543@gmail.com>
2023-10-06 07:37:48 +0300

    Update main.cpp

--- BM_search/main.cpp
+++ BM_search/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string.h>
+
+int BM_search(std::string text, std::string substring) {
+	int len_t = size(text);
+	int len_s = size(substring);
+	for (int i = 0; i < size(text);) {
+		for (int j = 0; j < len_s; j++) {
+
+		}
+	}
+}
 
 int main() {
+	int i;
 	std::string text, substring;
 	std::cout << "Enter the line with text: ";
 	std::cin >> text;
