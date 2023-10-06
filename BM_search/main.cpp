8a6db39454c388f2f8e27cc971c4d8dba97ec6ec
Stepa505 <stupanum2543@gmail.com>
2023-10-06 07:19:27 +0300

    БМ поиск

---
+++ BM_search/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	std::string text, substring;
+	std::cout << "Enter the line with text: ";
+	std::cin >> text;
+	std::cout << std::endl;
+	std::cout << "Enter the searched substring: ";
+	std::cin >> substring;
+}
