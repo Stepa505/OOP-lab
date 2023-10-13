ae3b96dd4c36dac8755fd75ab1cd058b2c4c2274
Stepa505 <stupanum2543@gmail.com>
2023-10-13 08:07:12 +0300

    Написал функцию, не работает, буду искать проблему

--- BM_search/main.cpp
+++ BM_search/main.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <string.h>
-#include "Array.h"
 
 int BM_search(std::string text, std::string substring) {
 	int len_t = size(text);
 	int len_s = size(substring);
 	int* shift1_array = new int[len_t];
 	int* shift2_array = new int[len_s];
-	shift2_array[len_s] = len_s;
+	shift2_array[len_s - 1] = len_s;
 	int n = len_s - 1;
+
 	for (int i = 0; i < len_s - 1; i++) {
 		shift2_array[i] = n;
 		n--;
 	}
+
 	for (int i = 0; i < len_t; i++) {
 		for (int j = 0; j < len_s; j++) {
 			if (text[i] == substring[j]) {
@@ -20,15 +21,32 @@ int BM_search(std::string text, std::string substring) {
 			}
 		}
 	}
-		
+	int i = 0, j = 0, k = 0;
+	for (; i < len_t; j++) {
+		if (text[i] == substring[j]){
+			if (j == len_s) {
+				k = i - len_s - 1;
+				break;
+			}
+			else i++;
+		}
+		else if (text[i] != substring[j]) {
+			i += shift2_array[j];
+			j = 0;
+		}
+		if (j == len_s) {
+			j = 0;
+		}
+	}
+	return k;
 }
 
 int main() {
-	int i;
 	std::string text, substring;
 	std::cout << "Enter the line with text: ";
 	std::cin >> text;
 	std::cout << std::endl;
 	std::cout << "Enter the searched substring: ";
 	std::cin >> substring;
+	std::cout << BM_search(text, substring);
 }
