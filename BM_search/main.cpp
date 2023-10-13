9b45e9775fc7bff83ff573191b2af780cf31f5c4
Stepa505 <stupanum2543@gmail.com>
2023-10-13 07:41:18 +0300

    начал писать функцию БМ поиска

--- BM_search/main.cpp
+++ BM_search/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <string.h>
+#include "Array.h"
 
 int BM_search(std::string text, std::string substring) {
 	int len_t = size(text);
 	int len_s = size(substring);
-	for (int i = 0; i < size(text);) {
+	int* shift1_array = new int[len_t];
+	int* shift2_array = new int[len_s];
+	shift2_array[len_s] = len_s;
+	int n = len_s - 1;
+	for (int i = 0; i < len_s - 1; i++) {
+		shift2_array[i] = n;
+		n--;
+	}
+	for (int i = 0; i < len_t; i++) {
 		for (int j = 0; j < len_s; j++) {
-
+			if (text[i] == substring[j]) {
+				shift1_array[i] = shift2_array[j];
+			}
 		}
 	}
+		
 }
 
 int main() {
