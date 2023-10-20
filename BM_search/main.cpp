d138ece82c64e540931549e256260700246bd324
stupa <stupa@DESKTOP-J89NNH6.local>
2023-10-20 08:05:07 +0300

    Полностью рабочий БМ-поиск, выполнены все задания

--- BM_search/main.cpp
+++ BM_search/main.cpp
@@ -14,7 +14,7 @@ int BM_search(std::string text, std::string substring, int *TAB, int start = 0, 
 	int len_s = substring.length();
 	int i, j, k = 0;
 	for (i = start + len_s - 1, j = len_s - 1; i <= end; i = k + TAB[text[k]]) {
-		for (k = i; j = len_s; j >= 0 && text[k] == substring[j];) {
+		for (k = i, j = len_s - 1; j >= 0 && text[k] == substring[j];) {
 			k--;
 			j--;
 		} 
@@ -25,9 +25,50 @@ int BM_search(std::string text, std::string substring, int *TAB, int start = 0, 
 	else return -1;
 }
 
+std::vector<int> BM_search_in_range(std::string text, std::string substring, int* TAB, int start, int end) {
+	std::vector<int> Entrance;
+	int	entrance = BM_search(text, substring, TAB, start, end);
+	if (entrance < 0) return Entrance;
+	while (entrance >= 0)
+	{
+		Entrance.push_back(entrance);
+		start = entrance + 1;
+		entrance = BM_search(text, substring, TAB, start, end);
+	}
+	return Entrance;
+}
 
+std::vector<int> BM_search_all(std::string text, std::string substring, int* TAB) {
+	return BM_search_in_range(text, substring, TAB, 0, text.length());
+}
 
 int main() {
-	std::string text = ":move_iterator is an iterator adaptor which behaves exactly like the underlying iterator", substring = "tor";
-
+	std::string text = "move_iterator is an iterator adaptor which behaves exactly like the underlying iterator", substring = "tor";
+	std::cout << "Text: " << text << ", substring: " << substring << std::endl;
+	int* TAB = TAB_generate(text, substring);
+	int first = BM_search(text, substring, TAB);
+	if (first < 0) std::cout << "No entrance";
+	else std::cout << "First entrance index: " << first << std::endl;
+	int start, end;
+	std::cout << "Enter the begining of search: ";
+	std::cin >> start;
+	std::cout << ". Enter the ending of search: ";
+	std::cin >> end;
+	std::vector<int> Entrance = BM_search_in_range(text, substring, TAB, start, end);
+	if (Entrance.size() == 0) std::cout << "No entrance in range(" << start << ";" << end << ")." << std::endl;
+	else{
+		std::cout << "Indexes of all entrance in range(" << start << ";" << end << "): ";
+		for (int el : Entrance)
+			std::cout << el << " ";
+		std::cout << "\n";
+	}
+	Entrance = BM_search_all(text, substring, TAB);
+	if (Entrance.size() == 0) std::cout << "No entrance in al text";
+	else {
+		std::cout << "Indexes of all entrance: ";
+		for (int el : Entrance)
+			std::cout << el << " ";
+		std::cout << "\n";
+	}
+	delete[] TAB;
 }
