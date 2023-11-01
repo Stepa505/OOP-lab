970fa085bac2d6ed85032e90dfeebd3850125f8a
stupa <stupa@DESKTOP-J89NNH6.local>
2023-11-01 18:33:49 +0300

    Функция сортировки на куче и заполнения рандомного массива

---
+++ Heap Sort/Heap_Sort.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+void rand_arr(int* arr, int& size) {
+	srand(time(0));
+	for (int i = 0; i < size; i++) {
+		arr[i] = 1 + rand() % 100;
+	}
+}
+
+void swap(int& a, int& b) {
+	int c;
+	c = b;
+	b = a;
+	a = c;
+}
+
+void Heap_Form(int* arr, int root, int bottom) {
+	int max_child, done = 0;
+	for (; root * 2 <= bottom && done != 1;) {
+		if (root * 2 == bottom) 
+			max_child = root * 2;
+		else if (arr[root * 2] > arr[root * 2 + 1]) 
+			max_child = root * 2;
+		else 
+			max_child = root * 2 + 1;
+		if (arr[root] < arr[max_child])
+			swap(arr[root], arr[max_child]);
+		else 
+			done = 1;
+	}
+}
+
+void Heap_Sort(int* arr, int size) {
+	for (int i = size / 2; i >= 0; i--) 
+		Heap_Form(arr, i, size - 1);
+	for (int i = size - 1; i >= 1; i--) {
+		swap(arr[0], arr[i]);
+		Heap_Form(arr, 0, i - 1);
+	}
+}
+
+int main() {
+	int* arr, size;
+	std::cin >> size;
+	arr = new int[size];
+
+
+}
