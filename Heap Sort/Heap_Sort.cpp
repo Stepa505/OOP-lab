7a65789913d75ed3c0579728529fa768092db000
stupa <stupa@DESKTOP-J89NNH6.local>
2023-11-02 15:11:47 +0300

    Нашел ошибку, потерял одну строчку в функции формирования кучи, теперь все работает

--- Heap Sort/Heap_Sort.cpp
+++ Heap Sort/Heap_Sort.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+//int max3(int& a, int& b, int& c) {
+//	int max;
+//	if (a >= b && a >= c) max = a;
+//	else if (b >= a && b >= c) max = b;
+//	else if (c >= b && c >= a) max = c;
+//	return max;
+//}
 
 void rand_arr(int* arr, int& size) {
 	srand(time(0));
@@ -16,26 +23,36 @@ void swap(int& a, int& b) {
 	a = c;
 }
 
+void arr_out(int* a, int n) {
+	for (int i = 0; i < n - 1; i++) {
+		std::cout << a[i] << " ,";
+	}
+	std::cout << a[n - 1] << ".";
+}
+
 void Heap_Form(int* arr, int root, int bottom) {
 	int max_child, done = 0;
-	for (; root * 2 <= bottom && done != 1;) {
+	while (root * 2 <= bottom && done == 0) {
 		if (root * 2 == bottom) 
 			max_child = root * 2;
 		else if (arr[root * 2] > arr[root * 2 + 1]) 
 			max_child = root * 2;
 		else 
 			max_child = root * 2 + 1;
-		if (arr[root] < arr[max_child])
+		if (arr[root] < arr[max_child]) {
 			swap(arr[root], arr[max_child]);
+			root = max_child;
+		}
 		else 
 			done = 1;
 	}
+	/*arr_out(arr, size);*/
 }
 
 void Heap_Sort(int* arr, int size) {
 	for (int i = size / 2; i >= 0; i--) 
 		Heap_Form(arr, i, size - 1);
-	for (int i = size - 1; i >= 1; i--) {
+	for (int i = size - 1; i > 0; i--) {
 		swap(arr[0], arr[i]);
 		Heap_Form(arr, 0, i - 1);
 	}
@@ -46,12 +63,6 @@ bool sort_check(int* arr, int& size) {
 		if (arr[i] > arr[i + 1]) return false;
 	}
 	return true;
-}
-
-void arr_out(int* a, int n) {
-	for (int i = 0; i < n; i++) {
-		std::cout << a[i] << " ,";
-	}
 }
 
 int main() {
@@ -65,7 +76,7 @@ int main() {
 	std::cout << std::endl;
 	if (choice == 0)
 		rand_arr(arr, size);
-	else {
+	else if (choice == 1) {
 		std::cout << "Enter array numbers: [ ";
 		for (int i = 0; i < size; i++) {
 			std::cin >> arr[i];
@@ -75,6 +86,10 @@ int main() {
 				std::cout << " ].";
 		}
 	}
+	std::cout << std::endl;
+	arr_out(arr, size);
+	std::cout << std::endl;
+	Heap_Form(arr, 0, size - 1);
 	Heap_Sort(arr, size);
 	arr_out(arr, size);
 	if (sort_check(arr, size)) {
