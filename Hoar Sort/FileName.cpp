d1d91586b189eb093fe8e0b0fabe3887609d067a
stupa <stupa@DESKTOP-J89NNH6.local>
2023-12-14 20:58:57 +0300

    Функция для перемещения файла в массив, и сортировка Хоара

--- Hoar Sort/FileName.cpp
+++ Hoar Sort/FileName.cpp
@@ -29,6 +29,41 @@ bool sort_check(std::vector<int>& arr)
 	return true;
 }
 
+void file_in_array(const char* name, std::vector<int>& arr)
+{
+	std::ifstream fin(name);
+	int n;
+	if (!fin.good())
+		std::cout << "File not found!" << std::endl;
+	else
+	{
+		while (fin >> n)
+			arr.push_back(n);
+	}
+	fin.close();
+}
+
+void hoar_sort(std::vector<int>& arr, const int l_lim, const int r_lim) {
+	if (l_lim >= r_lim) return;
+	int i = l_lim, j = r_lim, avg = arr[(l_lim + r_lim) / 2];
+	while (i <= j) {
+		while (arr[i] < avg)
+			i++;
+		while (arr[j] > avg)
+			j--;
+		if (i <= j)
+		{
+			std::swap(arr[i], arr[j]);
+			i++;
+			j--;
+		}
+	}
+	hoar_sort(arr, l_lim, j);
+	hoar_sort(arr, i, r_lim);
+}
+
+
+
 int main() {
 	for (int size = 10000; size <= 1000000; size *= 10) {
 		for (int range = 10; range <= 100000; range *= 100) {
